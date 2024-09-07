7d5985582d9946ce2a4b65bb73313197d5e5eea0
H-jfeng <766185347@qq.com>
2024-09-07 13:59:27 +0800

    Site updated: 2024-09-07 13:59:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,39 @@
-var posts=["2023/08/15/第七篇文章/","2023/07/19/第五篇文章/","2023/06/14/第三篇文章/","2023/07/18/第六篇文章/","2023/08/17/第十一篇文章/","2023/08/18/第十三篇文章/","2023/07/16/第十二篇文章/","2023/05/18/第十篇文章/","2023/02/19/第四篇文章/","2023/08/19/第二篇文章/","2023/07/15/第八篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/15/第七篇文章/","2023/06/14/第三篇文章/","2023/07/19/第五篇文章/","2023/08/17/第十一篇文章/","2023/08/19/第二篇文章/","2023/07/18/第六篇文章/","2023/08/18/第十三篇文章/","2023/05/18/第十篇文章/","2023/07/16/第十二篇文章/","2023/02/19/第四篇文章/","2023/07/15/第八篇文章/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[{"name":"安知鱼","link":"https://blog.anheyu.com/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"},{"name":"铭心石刻","link":"https://blog.kouseki.cn/","avatar":"https://npm.elemecdn.com/anzhiyu-blog-static@1.0.4/img/avatar.jpg","descr":"愿岁并谢，与友长兮","siteshot":"https://npm.elemecdn.com/anzhiyu-theme-static@1.1.6/img/blog.anheyu.com.jpg"}];
+    var refreshNum = 1;
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 4) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
