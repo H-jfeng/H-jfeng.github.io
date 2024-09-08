2236f799ac7de594cb6560019438503d486dc74d
H-jfeng <766185347@qq.com>
2024-09-08 17:48:39 +0800

    Site updated: 2024-09-08 17:48:29

--- js/sakura.js
+++ js/sakura.js
@@ -130,7 +130,7 @@ function startSakura() {
   document.getElementsByTagName("body")[0].appendChild(canvas);
   cxt = canvas.getContext("2d");
   var sakuraList = new SakuraList();
-  for (var i = 0; i < 10; i++) {
+  for (var i = 0; i < 15; i++) {
     //樱花数量
     var sakura, randomX, randomY, randomS, randomR, randomFnx, randomFny;
     randomX = getRandom("x");
